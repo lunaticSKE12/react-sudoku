6deb3162b870403c85b6b900fd95a423c537946d
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2019-07-22 19:53:50 +0700

    Set state initial game

--- src/App.js
+++ src/App.js
@@ -3,20 +3,54 @@ import logo from './logo.svg';
 import './App.css';
 
 class Cell extends Component {
-  state = {
-    number: 1
-  }
+
   render() {
     return (
       <div
-        onClick={(e) => {
-          this.setState({
-            number: (this.state.number + 1) % 5
-          });
+        onClick={e => {
+          if (this.props.isInitial) {
+            return;
+          }
+          this.props.onChange((this.props.number + 1) % 5);
         }}
         className={`cell ${this.props.isInitial ? 'initial' : ''}`}
       >
-        {this.state.number !== 0 && this.state.number}
+        {this.props.number !== 0 && this.props.number}
+      </div>
+    );
+  }
+}
+
+class Board extends Component {
+  state = {
+    board: [[1, 2, 3, 4], [3, 4, 0, 0], [2, 0, 4, 0], [4, 0, 0, 2]],
+    initial: [
+      [true, true, true, true],
+      [true, true, false, false],
+      [true, false, true, false],
+      [true, false, false, true]
+    ]
+  }
+  render() {
+    return (
+      <div className="board">
+        {
+          this.state.board.map((row, i) =>
+            row.map((number, j) =>
+              <Cell
+                key={`cell-${i}-${j}`}
+                isInitial={this.state.initial[i][j]}
+                number={number}
+                onChange={(newNumber) => {
+                  const { board } = this.state;
+                  board[i][j] = newNumber
+                  this.setState({
+                    board
+                  });
+                }}
+              />
+            ))
+        }
       </div>
     );
   }
@@ -25,26 +59,7 @@ class Cell extends Component {
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="board">
-          <Cell number={3} isInitial />
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} isInitial />
-          <Cell number={3} />
-          <Cell number={5} />
-          <Cell number={5} />
-          <Cell number={5} />
-          <Cell number={5} />
-          <Cell number={5} />
-          <Cell number={5} isInitial />
-          <Cell number={5} />
-          <Cell number={5} />
-          <Cell number={5} isInitial />
-          <Cell number={5} />
-        </div>
-      </div>
+      <div className="App"><Board></Board></div>
     );
   }
 }
